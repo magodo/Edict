17e8b2c98f961c5f42b666dbac90771b21f76a48
magodo <wztdyl@sina.com>
2015-04-19 21:04:38 +0800

    Modify waveio.py to set the default samplewidgth to int32.

--- lib/speech/waveio.py
+++ lib/speech/waveio.py
@@ -10,27 +10,15 @@ from ctypes import *
 import os
 import sys
 
-#------------------------------------------
-# Handler for ALSA configuration error log
-#------------------------------------------
-#ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
-#def py_error_handler(filename, line, function, err, fmt):
-#    #print "messages are yummy"
-#    pass
-#c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
-#asound = cdll.LoadLibrary('libasound.so')
-#asound.snd_lib_error_set_handler(c_error_handler)
-
 #-----------------------------------------
 # Functions
 #-----------------------------------------
-def keep_record(stopdict, channels = 1, rate = 8000):
+def keep_record(stopdict, channels = 1, rate = 8000, sampwidth = 4):
     """Keep record speech signal from voice until RecordStopFlag is set.
 
     :param channel: record channels(single or stereo)
     :param rate: sample rate
     """
-    sampwidth = 2   # 16bit
     chunk = 1024
     pa = pyaudio.PyAudio()
     stream = pa.open(format = pa.get_format_from_width(sampwidth),
@@ -42,14 +30,18 @@ def keep_record(stopdict, channels = 1, rate = 8000):
 
     while not stopdict['flag']:
         str_wave_data += stream.read(chunk)
-    wave_data = np.fromstring(str_wave_data, dtype = np.short)
+    if sampwidth == 2:
+        dtype = np.int16
+    elif sampwidth == 4:
+        dtype = np.int32
+    wave_data = np.fromstring(str_wave_data, dtype = dtype)
 
     stream.stop_stream()
     stream.close()
     pa.terminate
     return wave_data
 
-def recorde_t(duration, channels = 1, rate = 8000):
+def recorde_t(duration, channels = 1, rate = 8000, sampwidth = 4):
     """Record t(s) speech signal from voice
 
     :param duration: the record duraion
@@ -58,7 +50,6 @@ def recorde_t(duration, channels = 1, rate = 8000):
     :returns: numerical speech signal.
     """
     # 获取音频数据（二进制）
-    sampwidth = 2   # 16bit
     pa = pyaudio.PyAudio()
     stream = pa.open(format = pa.get_format_from_width(sampwidth),
                      channels = channels,
@@ -66,15 +57,19 @@ def recorde_t(duration, channels = 1, rate = 8000):
                      input = True)
     num_sample = duration * rate
     str_wave_data = stream.read(num_sample)
-    # 将音频数据转换为数组（S16型）
-    wave_data = np.fromstring(str_wave_data, dtype = np.short)
+    if sampwidth == 2:
+        dtype = np.int16
+    elif sampwidth == 4:
+        dtype = np.int32
+    # 将音频数据转换为数组
+    wave_data = np.fromstring(str_wave_data, dtype = dtype)
 
     stream.stop_stream()
     stream.close()
     pa.terminate()
     return wave_data
 
-def recorder_to_file(duration, filename, channels = 1, rate = 8000):
+def recorder_to_file(duration, filename, channels = 1, rate = 8000, sampwidth = 4):
     """Record speech signal from voice and write into a .wav file
 
     :param duration: the record duraion
@@ -85,7 +80,6 @@ def recorder_to_file(duration, filename, channels = 1, rate = 8000):
     """
 
     # 获取音频数据（二进制）
-    sampwidth = 2
     pa = pyaudio.PyAudio()
     stream = pa.open(format = pa.get_format_from_width(sampwidth),
                      channels = channels,
@@ -119,21 +113,25 @@ def wave_from_file(filename):
     wf = wave.open(filename, 'rb')
     samplerate = wf.getframerate()      # 获得采样率
     frame_num = wf.getnframes()         # 获得帧数
+    sampwidth = wf.getsampwidth()       # 获得采样点的长度
     str_wave_data = wf.readframes(frame_num)
-    wave_data = np.fromstring(str_wave_data, dtype = np.short)
+    if sampwidth == 2:
+        dtype = np.int16
+    elif sampwidth == 4:
+        dtype = np.int32
+    wave_data = np.fromstring(str_wave_data, dtype = dtype)
     wf.close()
     return (wave_data, samplerate)
 
-def echo(data, channels = 1, rate = 8000):
+def echo(data, channels = 1, rate = 8000, sampwidth = 4):
     """Echo speech signal from wave data.
 
     :param data: the wave data
     :param channel: wave channels(single or stereo)
     :param rate: sample rate
     :returns: None"""
-    #sampwidth = 2   # 16bit
     pa = pyaudio.PyAudio()
-    stream = pa.open(format = pyaudio.paInt16,
+    stream = pa.open(format = pyaudio.get_format_from_width(sampwidth),
                      channels = channels,
                      rate = rate,
                      output = True)
